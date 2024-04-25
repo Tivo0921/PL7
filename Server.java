10e254eec2f9d1954638bf11cacf735c8da3bf81
Tivo0921 <shun020921@icloud.com>
2024-04-25 21:53:37 +0900

    変数関数宣言　池田

---
+++ Server.java
@@ -0,0 +1,26 @@
+public class Server {
+    // 変数宣言
+    int[][] clientConnectionState = new int[1][2];
+    String playerName = new String();
+    String password = new String();
+    String playerID = new String();
+    boolean loginSuccess = false;
+    int[][] playRecord = new int[1][3];
+    int roomID = 0;
+
+    public int[][] connectClient(){
+
+    }
+
+    public int[][] checkClientConnection(void){
+
+    }
+
+    public boolean checkLoginInformation(String){
+
+    }
+
+    public int[][] transferPlayRecord(int[][]){
+        
+    }
+}
