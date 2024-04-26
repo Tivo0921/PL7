339397e378ab91e79b08ef06bf4f143109375bf2
otanisae <otani-sae-bg@ynu.jp>
2024-04-26 15:32:52 +0900

    変数・メソッドを定義

--- Player.java
+++ Player.java
@@ -1,1 +1,60 @@
-// 担当：さえ
+public class Player{
+
+    String playerName; // プレイヤー名
+    int turn; // 先手後手(白黒)情報
+    int roomId; // ルームID
+    String password; // パスワード
+    String playerId; // プレイヤのID
+    int[][] stones = new int[8][8]; // 石を置いた場所
+    int[][] gameRecord = new int[1][3]; // 対戦成績
+
+    // アプリ立ち上げ
+    public void setupApp(){
+
+    }
+
+    // 対戦成績の閲覧
+    public void displayPlayRecord(){
+
+    }
+
+    // ルームの作成
+    public void makeRoom(){
+
+    }
+
+    // ルームIDを入力
+    public void inputRoomId(){
+
+    }
+
+    // プレイヤ名を受付
+    public void acceptPlayerId(){
+
+    }
+
+    // プレイヤ名を取得
+    public string getPlayerId(){
+
+    }
+
+    // 先手後手(白黒)情報を取得
+    public void getTurn(){
+
+    }
+
+    // 盤面の石の位置情報を送信
+    public void sendPosition(){
+
+    }
+
+    // 盤面の石の位置情報を受信
+    public void getPosition(){
+
+    }
+
+    // 投了受付
+    public boolean setGame(){
+
+    }
+}
