f23d512704fe6e585a71628904f5abb79169aad0
Tivo0921 <shun020921@icloud.com>
2024-04-26 17:34:20 +0900

    4/26対面会

--- Server1.java
+++ Server1.java
@@ -1,25 +1,95 @@
-public class Server {
+import java.io.*;
+import java.net.*;
+
+//工数6,進捗2
+public class Server1 {
     // 変数宣言
-    int[][] clientConnectionState = new int[1][2];
-    String playerName = new String();
-    String password = new String();
-    String playerID = new String();
-    boolean loginSuccess = false;
-    int[][] playRecord = new int[1][3];
-    int roomID = 0;
+    public int[][] clientConnectionState = new int[1][2];
+    public String playerName = new String();
+    public String password = new String();
+    public int playerID = 0;
+    public boolean loginSuccess = false;
+    public int[][] playRecord = new int[1][3];
+    public int roomID = 0;// 0から昇順
+    public int maxRoomID = 0;// 最大のルームID(ルームID数-1)
+    // 工数2,進捗1
 
-    public int[][] connectClient(){
+    public int[][] connectClient(){//一旦チャットする仕様にしてある
+        //TCPポートを指定してサーバソケットを作成
+        try(ServerSocket server = new ServerSocket(10000)){
+            while(true){
+                try{
+                    //クライアントからの接続待ち受け
+                    Socket sc = server.accept();
+                    System.out.println("クライアントからの接続がありました");
 
+                    BufferedReader reader = null;
+                    PrintWriter writer = null;
+
+                    //クライアントからの接続ごとにスレッドで通信処理を実行
+                    try{
+                        //クライアントからの受け取り用
+                        reader = new BufferedReader(new InputStreamReader(sc.getInputStream()));
+                        //クライアントへの送信用
+                        writer = new PrintWriter(sc.getOutputStream(),true);
+                        //クライアントから「exit」が入力されるまで無限ループ
+                        String line = null;
+                        while(true){
+                            line = reader.readLine();
+                            //クライアントから送信されたメッセージを取得
+                            if(line.equals("exit")){
+                                break;
+                            }
+
+                            System.out.println("クライアントからのメッセージ＝"+line);
+                            writer.println("Please Input");
+                        }
+
+                    }catch (Exception e){
+                        ex.printStackTrace();
+                    }finally{
+                        //リソースの解放
+                        if(reader!=null){
+                            reader.close();
+                        }
+                        if(writer!=null){
+                                writer.close();
+                        }
+                        if(sc!=null){
+                            sc.close();
+                        }
+                    }
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                    break;
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public int[][] checkClientConnection(void){
+    // 工数1,進捗1
+    public int[][] checkClientConnection(void){//clientCOnnectionState[][]を返す
+        for(int i = 0;i<maxRoomID;i++){
+            for(int j = 0; j < 1 ; j++){
+                if(clientConnectionState[i][j]){
+                    System.out.println(i +"," j + ":接続中");
+                }
+                else{
+                    System.out.println(i +"," j + ":接続切断");
+                }
 
+            }
+        }
     }
 
+    // 工数2,進捗0
     public boolean checkLoginInformation(String){
 
     }
 
+    // 工数1,進捗0
     public int[][] transferPlayRecord(int[][]){
         
     }
